98db58a939a817a0cdee581a76cb939c21930e0d
Suraj kumar <devilsuraj9641@gmail.com>
2024-02-23 02:20:22 +0530

    navbar

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { IoAccessibility } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import {FiShoppingCart,FiLogIn} from "react-icons/fi";
+import {FaUser} from "react-icons/fa";
+
+function Header({isAuthenticated=false}) {
+  return (
+    <nav>
+      <div>
+        <IoAccessibility />
+      </div>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+        <Link to="/cart"><FiShoppingCart/></Link>
+
+        <Link to={isAuthenticated?"/me":"/login"}>
+          {isAuthenticated ? <FaUser/> :<FiLogIn/>}
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
