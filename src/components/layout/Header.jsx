996eeb9f0db913fe4b0bd158aa465c5c816b343e
Suraj kumar <devilsuraj9641@gmail.com>
2024-02-25 20:00:08 +0530

    nd:]

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,13 +3,14 @@ import { IoAccessibility } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import {FiShoppingCart,FiLogIn} from "react-icons/fi";
 import {FaUser} from "react-icons/fa";
+import {motion} from "framer-motion"
 
 function Header({isAuthenticated=false}) {
   return (
     <nav>
-      <div>
+      <motion.div initial={{x:"-100%"}}whileInView={{x:0}}>
         <IoAccessibility />
-      </div>
+      </motion.div>
       <div>
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
